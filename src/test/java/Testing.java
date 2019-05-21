42ff0d6c80fcb2c3303e26af747257fbe932da29
Sjard Willems <sjard.willems@student.hu.nl>
2019-05-21 22:57:45 +0200

    Sonar toegvoeged en prob meerdere dingen zoals .idea

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,4 +1,5 @@
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
@@ -6,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 public class Testing {
 
     @Test
+    @Disabled("Test to check if wercker works.")
     public void test_fail() {
         fail("Let's test the failing unit test");
     }
