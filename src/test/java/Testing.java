7621c7616e8e690f059513dcb5601ede110e7109
Sjard Willems <sjard.willems@student.hu.nl>
2019-05-21 22:02:54 +0200

    failing test added

---
+++ src/test/java/Testing.java
@@ -0,0 +1,12 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class Testing {
+
+    @Test
+    public void test_fail() {
+        fail("Let's test the failing unit test");
+    }
+}
