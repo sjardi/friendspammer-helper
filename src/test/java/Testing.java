62bcff5500985999a0496c34dd41501cc58195ba
Sjard Willems <sjard.willems@student.hu.nl>
2019-05-24 19:09:58 +0200

    .gitignore toegevoegd met config.properties

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,12 +1,7 @@
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class Testing {
 
@@ -16,14 +11,6 @@ public class Testing {
         fail("Let's test the failing unit test");
     }
 
-    @Test
-    public void testPropertiesMethod() throws IOException {
-        Properties props = new Properties();
-        InputStream input = new FileInputStream("src/config/config.properties");
-        props.load(input);
-        System.out.print( props.getProperty("password"));
-        assertEquals(props.getProperty("password"), "45e7c5f4925a9d");
-    }
 
 }
 
